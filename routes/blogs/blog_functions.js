d34c11c309bc9a22b4b702c5a0316aa439884536
Kingsley Tagbo <kingsleytagbo@gmail.com>
2022-12-13 07:36:23 -0600

    post by slug

--- routes/blogs/blog_functions.js
+++ routes/blogs/blog_functions.js
@@ -33,7 +33,7 @@ const BlogFunctions = {
     },
 
     /*
-        SELECTS a singLe Blog & associated roles from SQL Server
+        SELECTS a singLe Blog from SQL Server
     */
     getItem: async (config, privateKeyID, id) => {
 
@@ -57,98 +57,124 @@ const BlogFunctions = {
         }
     },
 
-        /*
-        Creates a singLe Blog & associated roles in SQL Server
-    */
-        createItem: async (config, privateKeyID, user, data) => {
-            privateKeyID = privateKeyID ? String(privateKeyID).trim().toLowerCase() : privateKeyID;
-    
-            try {
-                await sql.connect(config);
-                const request = new sql.Request();
-                request.input('Name', sql.NVarChar(125), data.Name);
-                request.input('Description', sql.NVarChar(), data.Description);
-                request.input('Slug', sql.NVarChar(256), (data.Slug || ''));
-                request.input('BlogType', sql.NVarChar(), (data.BlogType || ''));
-                request.input('PostDate', sql.DateTime2, data.PostDate);
-                request.input('SortOrder', sql.Int, (data.SortOrder || 0));
-                request.input('Category', sql.NVarChar(), (data.Category || ''));
-                request.input('Tags', sql.NVarChar(), (data.Tags || ''));
-                request.input('PostSummary', sql.NVarChar(), (data.PostSummary || {}));
-                request.input('ITCC_UserID', sql.NVarChar(), user.ITCC_UserID);
-                request.input('SiteID', sql.NVarChar(), data.ITCC_WebsiteID);
-                request.input('ITCC_StatusID', sql.NVarChar(), 2);
-                request.input('CreateDate', sql.DateTime, data.CreateDate);
-                request.input('ModifyDate', sql.DateTime, data.ModifyDate);
-                request.input('ModifyUserID', sql.NVarChar(), data.ModifyUserID);
-                request.input('RoleName', sql.NVarChar(), data.RoleName);
-                request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
-    
-                let query = ' SELECT @SiteID = ITCC_WebsiteID FROM ITCC_WEBSITE (NOLOCK) WHERE (PrivateKeyID = @PrivateKeyID) ';
-                query += ' BEGIN TRAN; ';
-                query += ' INSERT INTO ITCC_BLOG (Name, Description, Slug, BlogType, Permalink, PostDate, Category, Tags, PostSummary, ';
-                query += ' ITCC_UserID, ITCC_WebsiteID, ITCC_StatusID, CreateDate, ModifyDate, ModifyUserID, RoleName, SortOrder )';
-                query += '  VALUES (@Name, @Description, @Slug, @BlogType, NEWID(), @PostDate, @Category, @Tags, @PostSummary, ';
-                query += ' @ITCC_UserID, @SiteID, @ITCC_StatusID, @CreateDate, @ModifyDate, @ModifyUserID, @RoleName, @SortOrder )';
-        
-                query += ' COMMIT TRANSACTION;';
-                query += ' SELECT SCOPE_IDENTITY() NEWID;';
-        
-                const authResult = await request.query(query);
-                const result = (authResult && authResult.recordset && authResult.recordset.length > 0) ? authResult.recordset[0] : null;
-    
-                return result;
-    
-            } catch (err) {
-                throw err
-            }
-        },
+    /*
+    SELECTS a singLe Blog By Slug from SQL Server
+*/
+    getItemBySlug: async (config, privateKeyID, slug) => {
+
+        try {
+            await sql.connect(config);
+            let query = ' SELECT DISTINCT US.* ';
+            query += ' FROM [ITCC_Blog] US ';
+            query += ' JOIN [ITCC_Website] WS (NOLOCK) ON (US.ITCC_WebsiteID = WS.ITCC_WebsiteID) ';
+            query += ' WHERE ( ' +
+                ' (US.Slug = @Slug) AND (WS.PrivateKeyID = @PrivateKeyID) ' +
+                ') ';
+
+            const request = new sql.Request();
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+            request.input('Slug', sql.NVarChar(256), slug);
+            const result = await request.query(query);
+            return result;
+
+        } catch (err) {
+            throw err
+        }
+    },
+
+
+    /*
+    Creates a singLe Blog & associated roles in SQL Server
+*/
+    createItem: async (config, privateKeyID, user, data) => {
+        privateKeyID = privateKeyID ? String(privateKeyID).trim().toLowerCase() : privateKeyID;
+
+        try {
+            await sql.connect(config);
+            const request = new sql.Request();
+            request.input('Name', sql.NVarChar(125), data.Name);
+            request.input('Description', sql.NVarChar(), data.Description);
+            request.input('Slug', sql.NVarChar(256), (data.Slug || ''));
+            request.input('BlogType', sql.NVarChar(), (data.BlogType || ''));
+            request.input('PostDate', sql.DateTime2, data.PostDate);
+            request.input('SortOrder', sql.Int, (data.SortOrder || 0));
+            request.input('Category', sql.NVarChar(), (data.Category || ''));
+            request.input('Tags', sql.NVarChar(), (data.Tags || ''));
+            request.input('PostSummary', sql.NVarChar(), (data.PostSummary || {}));
+            request.input('ITCC_UserID', sql.NVarChar(), user.ITCC_UserID);
+            request.input('SiteID', sql.NVarChar(), data.ITCC_WebsiteID);
+            request.input('ITCC_StatusID', sql.NVarChar(), 2);
+            request.input('CreateDate', sql.DateTime, data.CreateDate);
+            request.input('ModifyDate', sql.DateTime, data.ModifyDate);
+            request.input('ModifyUserID', sql.NVarChar(), data.ModifyUserID);
+            request.input('RoleName', sql.NVarChar(), data.RoleName);
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+
+            let query = ' SELECT @SiteID = ITCC_WebsiteID FROM ITCC_WEBSITE (NOLOCK) WHERE (PrivateKeyID = @PrivateKeyID) ';
+            query += ' BEGIN TRAN; ';
+            query += ' INSERT INTO ITCC_BLOG (Name, Description, Slug, BlogType, Permalink, PostDate, Category, Tags, PostSummary, ';
+            query += ' ITCC_UserID, ITCC_WebsiteID, ITCC_StatusID, CreateDate, ModifyDate, ModifyUserID, RoleName, SortOrder )';
+            query += '  VALUES (@Name, @Description, @Slug, @BlogType, NEWID(), @PostDate, @Category, @Tags, @PostSummary, ';
+            query += ' @ITCC_UserID, @SiteID, @ITCC_StatusID, @CreateDate, @ModifyDate, @ModifyUserID, @RoleName, @SortOrder )';
+
+            query += ' COMMIT TRANSACTION;';
+            query += ' SELECT SCOPE_IDENTITY() NEWID;';
+
+            const authResult = await request.query(query);
+            const result = (authResult && authResult.recordset && authResult.recordset.length > 0) ? authResult.recordset[0] : null;
+
+            return result;
+
+        } catch (err) {
+            throw err
+        }
+    },
 
     /*
         Updates a singLe Blog's information on SQL Server
     */
-        updateItem: async (config, privateKeyID, user, data) => {
-            privateKeyID = privateKeyID ? String(privateKeyID).trim().toLowerCase() : privateKeyID;
-    
-            try {
-                await sql.connect(config);
-                const request = new sql.Request();
-                request.input('Name', sql.NVarChar(125), data.Name);
-                request.input('Description', sql.NVarChar(), data.Description);
-                request.input('Slug', sql.NVarChar(256), (data.Slug || ''));
-                request.input('BlogType', sql.NVarChar(), (data.BlogType || ''));
-                request.input('PostDate', sql.DateTime2, data.PostDate);
-                request.input('SortOrder', sql.Int, (data.SortOrder || 0));
-                request.input('Category', sql.NVarChar(), (data.Category || ''));
-                request.input('Tags', sql.NVarChar(), (data.Tags || ''));
-                request.input('PostSummary', sql.NVarChar(), (data.PostSummary || {}));
-                request.input('ITCC_BlogID', sql.Int, data.ITCC_BlogID);
-                request.input('ITCC_UserID', sql.Int, user.ITCC_UserID);
-                request.input('SiteID', sql.NVarChar(), data.ITCC_WebsiteID);
-                request.input('ITCC_StatusID', sql.NVarChar(), 2);
-                request.input('CreateDate', sql.DateTime, data.CreateDate);
-                request.input('ModifyDate', sql.DateTime, data.ModifyDate);
-                request.input('ModifyUserID', sql.NVarChar(), data.ModifyUserID);
-                request.input('RoleName', sql.NVarChar(), data.RoleName);
-                request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
-    
-                let query = ' ';
-                query += ' BEGIN TRAN; ';
-                query += ' UPDATE ITCC_BLOG SET Name=@Name, Description=@Description, Slug=@Slug, BlogType=@BlogType, ';
-                query += ' PostDate = @PostDate, Category=@Category, Tags=@Tags, ITCC_StatusID=@ITCC_StatusID, ModifyDate = @ModifyDate, SortOrder = @SortOrder ';
-                query += ' WHERE ITCC_BlogID = @ITCC_BlogID; '; 
-                query += ' COMMIT TRANSACTION;';
-                query += ' SELECT @@ROWCOUNT;';
-        
-                const authResult = await request.query(query);
-                const result = data;
-    
-                return result;
-    
-            } catch (err) {
-                throw err
-            }
-        },
+    updateItem: async (config, privateKeyID, user, data) => {
+        privateKeyID = privateKeyID ? String(privateKeyID).trim().toLowerCase() : privateKeyID;
+
+        try {
+            await sql.connect(config);
+            const request = new sql.Request();
+            request.input('Name', sql.NVarChar(125), data.Name);
+            request.input('Description', sql.NVarChar(), data.Description);
+            request.input('Slug', sql.NVarChar(256), (data.Slug || ''));
+            request.input('BlogType', sql.NVarChar(), (data.BlogType || ''));
+            request.input('PostDate', sql.DateTime2, data.PostDate);
+            request.input('SortOrder', sql.Int, (data.SortOrder || 0));
+            request.input('Category', sql.NVarChar(), (data.Category || ''));
+            request.input('Tags', sql.NVarChar(), (data.Tags || ''));
+            request.input('PostSummary', sql.NVarChar(), (data.PostSummary || {}));
+            request.input('ITCC_BlogID', sql.Int, data.ITCC_BlogID);
+            request.input('ITCC_UserID', sql.Int, user.ITCC_UserID);
+            request.input('SiteID', sql.NVarChar(), data.ITCC_WebsiteID);
+            request.input('ITCC_StatusID', sql.NVarChar(), 2);
+            request.input('CreateDate', sql.DateTime, data.CreateDate);
+            request.input('ModifyDate', sql.DateTime, data.ModifyDate);
+            request.input('ModifyUserID', sql.NVarChar(), data.ModifyUserID);
+            request.input('RoleName', sql.NVarChar(), data.RoleName);
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+
+            let query = ' ';
+            query += ' BEGIN TRAN; ';
+            query += ' UPDATE ITCC_BLOG SET Name=@Name, Description=@Description, Slug=@Slug, BlogType=@BlogType, ';
+            query += ' PostDate = @PostDate, Category=@Category, Tags=@Tags, ITCC_StatusID=@ITCC_StatusID, ModifyDate = @ModifyDate, SortOrder = @SortOrder ';
+            query += ' WHERE ITCC_BlogID = @ITCC_BlogID; ';
+            query += ' COMMIT TRANSACTION;';
+            query += ' SELECT @@ROWCOUNT;';
+
+            const authResult = await request.query(query);
+            const result = data;
+
+            return result;
+
+        } catch (err) {
+            throw err
+        }
+    },
 
     /*
         Deletes a singLe non-admin Blog's information on SQL Server  
--- routes/blogs/index.js
+++ routes/blogs/index.js
@@ -6,9 +6,9 @@ const LoginFunctions = require('../login/login_functions');
 
 const BlogFactory = {
     Fields: {
-        Name: '', Description: '', Slug: '', BlogType: '', Permalink: '', PostDate: '', PostSummary:'',
-        Category: '', Tags:'', ITCC_UserID: -1, ITCC_WebsiteID: -1, ITCC_StatusID: -1, ITCC_BlogID: 0,
-        CreateDate: new Date(), ModifyDate: new Date(), CreateUserID:-1, ModifyUserID: -1, RoleName: 'subscriber'
+        Name: '', Description: '', Slug: '', BlogType: '', Permalink: '', PostDate: '', PostSummary: '',
+        Category: '', Tags: '', ITCC_UserID: -1, ITCC_WebsiteID: -1, ITCC_StatusID: -1, ITCC_BlogID: 0,
+        CreateDate: new Date(), ModifyDate: new Date(), CreateUserID: -1, ModifyUserID: -1, RoleName: 'subscriber'
     }
     , Set: function (value) {
         this.Fields = Object.assign(this.Fields, value);
@@ -28,7 +28,7 @@ router.get("/:siteid/page/:pagenum/:pagesize", async function (request, response
     const authToken = LoginFunctions.getAuthenticationToken(request);
     const authID = authToken || (request.headers.authid);
     const pageNum = (request.params.pagenum) ? request.params.pagenum : 1;
-    const pageSize = (request.params.pagesize) ? request.params.pagesize : 10; 
+    const pageSize = (request.params.pagesize) ? request.params.pagesize : 10;
     const offset = (pageNum - 1) * pageSize;
 
     const config = configs.find(siteid); //(c => c.privateKeyID === siteid);
@@ -56,8 +56,29 @@ router.get("/:siteid/:id", async function (request, response) {
     const config = configs.find(siteid); //(c => c.privateKeyID === siteid);
     const roleNames = await LoginFunctions.getUserRolesByAuthToken(config, siteid, authID);
     const authResult = await blogs.getItem(config, siteid, id);
-    const result =  (authResult.recordset && (authResult.recordset.length > 0))
-    ? authResult.recordset[0] : null;
+    const result = (authResult.recordset && (authResult.recordset.length > 0))
+        ? authResult.recordset[0] : null;
+
+    if (roleNames && roleNames.indexOf('admin') > -1) {
+        return response.send(result);
+    }
+    else {
+        return response.send(result);
+    }
+});
+
+// get one blog by slug
+router.get("/:siteid/slug/:id", async function (request, response) {
+    const siteid = request.params.siteid;
+    const authToken = LoginFunctions.getAuthenticationToken(request);
+    const authID = authToken || (request.headers.authid);
+    const id = request.params.id;
+
+    const config = configs.find(siteid); //(c => c.privateKeyID === siteid);
+    const roleNames = await LoginFunctions.getUserRolesByAuthToken(config, siteid, authID);
+    const authResult = await blogs.getItemBySlug(config, siteid, id);
+    const result = (authResult.recordset && (authResult.recordset.length > 0))
+        ? authResult.recordset[0] : null;
 
     if (roleNames && roleNames.indexOf('admin') > -1) {
         return response.send(result);
@@ -82,16 +103,16 @@ router.post("/:siteid", async function (request, response) {
         BlogFactory.Set(request.body);
         const dataValues = BlogFactory.Get();
 
-        if(!dataValues.Slug || dataValues.Slug.trim().length === 0){
+        if (!dataValues.Slug || dataValues.Slug.trim().length === 0) {
             dataValues.Slug = SharedFunctions.slugify(dataValues.Name);
         }
-        
+
         const authResult = await blogs.createItem(config, siteid, authUser, dataValues);
-        const result =  authResult;
+        const result = authResult;
         return response.send(result);
     }
     else {
-        return response.status(401).send({error: 'you\'re not authorized to access this'});
+        return response.status(401).send({ error: 'you\'re not authorized to access this' });
     }
 
 });
@@ -110,16 +131,16 @@ router.put("/:siteid/:id", async function (request, response) {
         BlogFactory.Set(request.body);
         const dataValues = BlogFactory.Get();
 
-        if(!dataValues.Slug || dataValues.Slug.trim().length === 0){
+        if (!dataValues.Slug || dataValues.Slug.trim().length === 0) {
             dataValues.Slug = SharedFunctions.slugify(dataValues.Name);
         }
 
         const authResult = await blogs.updateItem(config, siteid, authUser, dataValues);
-        const result =  authResult.recordset;
+        const result = authResult.recordset;
         return response.send(result);
     }
     else {
-        return response.status(401).send({error: 'you\'re not authorized to access this'});
+        return response.status(401).send({ error: 'you\'re not authorized to access this' });
     }
 });
 
@@ -138,7 +159,7 @@ router.delete("/:siteid/:id", async function (request, response) {
         return response.send(id);
     }
     else {
-        return response.status(401).send({error: 'you\'re not authorized to access this'});
+        return response.status(401).send({ error: 'you\'re not authorized to access this' });
     }
 });
 
