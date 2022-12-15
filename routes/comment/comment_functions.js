877b6200e61c3de55884fefbacc6ed35097d65fc
Kingsley Tagbo <kingsleytagbo@gmail.com>
2022-12-15 16:49:31 -0600

    comments

---
+++ routes/comment/comment_functions.js
@@ -0,0 +1,210 @@
+
+const sql = require("mssql");
+
+const CommentFunctions = {
+
+    /*
+            SELECT a paged list of Comments & associated roles from SQL Server
+    */
+    getItems: async (config, privateKeyID, offset, pageSize) => {
+        try {
+            await sql.connect(config);
+
+            let query = ' SELECT DISTINCT US.* ';
+            query += ' FROM [ITCC_Comment] US ';
+            query += ' JOIN [ITCC_Blog] BG (NOLOCK) ON (US.ITCC_PostID = BG.ITCC_BlogID) ';
+            query += ' JOIN [ITCC_Website] WS (NOLOCK) ON (US.ITCC_WebsiteID = WS.ITCC_WebsiteID) ';
+            query += ' WHERE ( (WS.PrivateKeyID = @PrivateKeyID) ) ';
+            query += ' ORDER BY US.ModifyDate Desc ';
+            query += ' OFFSET @Offset ROWS ';
+            query += ' FETCH NEXT @PageSize ROWS ONLY ';
+
+            const request = new sql.Request();
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+            request.input('Offset', sql.Int, offset);
+            request.input('PageSize', sql.Int, pageSize);
+
+            const result = await request.query(query);
+            return result;
+
+        } catch (err) {
+            //console.log({getComments: err});
+            throw err
+        }
+    },
+
+    /*
+        SELECTS a singLe Comment from SQL Server
+    */
+    getItem: async (config, privateKeyID, id) => {
+
+        try {
+            await sql.connect(config);
+            let query = ' SELECT DISTINCT US.* ';
+            query += ' FROM [ITCC_Comment] US ';
+            query += ' JOIN [ITCC_Website] WS (NOLOCK) ON (US.ITCC_WebsiteID = WS.ITCC_WebsiteID) ';
+            query += ' WHERE ( ' +
+                ' (US.ITCC_CommentID = @ID) AND (WS.PrivateKeyID = @PrivateKeyID) ' +
+                ') ';
+
+            const request = new sql.Request();
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+            request.input('ID', sql.Int, id);
+            const result = await request.query(query);
+            return result;
+
+        } catch (err) {
+            throw err
+        }
+    },
+
+    /*
+    SELECTS a singLe Comment By Slug from SQL Server
+*/
+    getItemBySlug: async (config, privateKeyID, slug) => {
+
+        try {
+            await sql.connect(config);
+            let query = ' SELECT DISTINCT US.* ';
+            query += ' FROM [ITCC_Comment] US ';
+            query += ' JOIN [ITCC_Website] WS (NOLOCK) ON (US.ITCC_WebsiteID = WS.ITCC_WebsiteID) ';
+            query += ' WHERE ( ' +
+                ' (US.Slug = @Slug) AND (WS.PrivateKeyID = @PrivateKeyID) ' +
+                ') ';
+
+            const request = new sql.Request();
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+            request.input('Slug', sql.NVarChar(256), slug);
+            const result = await request.query(query);
+            return result;
+
+        } catch (err) {
+            throw err
+        }
+    },
+
+
+    /*
+    Creates a singLe Comment & associated roles in SQL Server
+*/
+    createItem: async (config, privateKeyID, user, data) => {
+        privateKeyID = privateKeyID ? String(privateKeyID).trim().toLowerCase() : privateKeyID;
+
+        try {
+            await sql.connect(config);
+            const request = new sql.Request();
+            request.input('Name', sql.NVarChar(125), data.Name);
+            request.input('Description', sql.NVarChar(), data.Description);
+            request.input('Slug', sql.NVarChar(256), (data.Slug || ''));
+            request.input('CommentType', sql.NVarChar(), (data.CommentType || ''));
+            request.input('PostDate', sql.DateTime2, data.PostDate);
+            request.input('SortOrder', sql.Int, (data.SortOrder || 0));
+            request.input('Category', sql.NVarChar(), (data.Category || ''));
+            request.input('Tags', sql.NVarChar(), (data.Tags || ''));
+            request.input('PostSummary', sql.NVarChar(), (data.PostSummary || {}));
+            request.input('ITCC_UserID', sql.Int, user.ITCC_UserID);
+            request.input('SiteID', sql.Int, data.ITCC_WebsiteID);
+            request.input('ITCC_StatusID', sql.Int, 2);
+            request.input('CreateDate', sql.DateTime, data.CreateDate);
+            request.input('ModifyDate', sql.DateTime, data.ModifyDate);
+            request.input('ModifyUserID', sql.Int, data.ModifyUserID);
+            request.input('RoleName', sql.NVarChar(), data.RoleName);
+            request.input('Author', sql.NVarChar(256), user.FirstName + ' ' + user.LastName);
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+
+            let query = ' SELECT @SiteID = ITCC_WebsiteID FROM ITCC_WEBSITE (NOLOCK) WHERE (PrivateKeyID = @PrivateKeyID) ';
+            query += ' BEGIN TRAN; ';
+            query += ' INSERT INTO ITCC_Comment (Name, Description, Slug, CommentType, Permalink, PostDate, Category, Tags, PostSummary, ';
+            query += ' ITCC_UserID, ITCC_WebsiteID, ITCC_StatusID, CreateDate, ModifyDate, ModifyUserID, RoleName, SortOrder, Author )';
+            query += '  VALUES (@Name, @Description, @Slug, @CommentType, NEWID(), @PostDate, @Category, @Tags, @PostSummary, ';
+            query += ' @ITCC_UserID, @SiteID, @ITCC_StatusID, @CreateDate, @ModifyDate, @ModifyUserID, @RoleName, @SortOrder, @Author )';
+
+            query += ' COMMIT TRANSACTION;';
+            query += ' SELECT SCOPE_IDENTITY() NEWID;';
+
+            const authResult = await request.query(query);
+            const result = (authResult && authResult.recordset && authResult.recordset.length > 0) ? authResult.recordset[0] : null;
+
+            return result;
+
+        } catch (err) {
+            throw err
+        }
+    },
+
+    /*
+        Updates a singLe Comment's information on SQL Server
+    */
+    updateItem: async (config, privateKeyID, user, data) => {
+        privateKeyID = privateKeyID ? String(privateKeyID).trim().toLowerCase() : privateKeyID;
+
+        try {
+            await sql.connect(config);
+            const request = new sql.Request();
+
+            request.input('ITCC_CommentID', sql.Int, data.ITCC_CommentID);
+            request.input('SiteID', sql.NVarChar(), data.ITCC_WebsiteID);
+            request.input('ITCC_StatusID', sql.NVarChar(), 2);
+            request.input('ITCC_UserID', sql.Int, user.ITCC_UserID);
+            request.input('ITCC_PostID', sql.Int, data.ITCC_PostID);
+            request.input('ReplyPostID', sql.Int, data.ReplyPostID);
+
+            request.input('CommentTitle', sql.NVarChar(384), data.CommentTitle || '');
+            request.input('CommentDetail', sql.NVarChar(), data.CommentDetail);
+            request.input('CommentFullName', sql.NVarChar(128), (data.CommentFullName || ''));
+
+            request.input('SortOrder', sql.Int, (data.SortOrder || 0));
+            request.input('ReplyLevel', sql.Int, (data.ReplyLevel || 0));
+
+            request.input('ModifyDate', sql.DateTime, data.ModifyDate);
+            request.input('ModifyAccountID', sql.NVarChar(), data.ModifyAccountID);
+
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+
+            let query = ' ';
+            query += ' BEGIN TRAN; ';
+            query += ' UPDATE ITCC_Comment SET CommentTitle=@CommentTitle, CommentDetail=@CommentDetail, CommentFullName=@CommentFullName,';
+            query += ' ITCC_StatusID=@ITCC_StatusID, ReplyLevel=@ReplyLevel, ReplyPostID=@ReplyPostID, SortOrder = @SortOrder, ';
+            query += ' ModifyDate = getDate(), ModifyAccountID=@ModifyAccountID ';
+            query += ' WHERE ITCC_CommentID = @ITCC_CommentID; ';
+            query += ' COMMIT TRANSACTION;';
+            query += ' SELECT @@ROWCOUNT;';
+
+            const authResult = await request.query(query);
+            const result = data;
+
+            return result;
+
+        } catch (err) {
+            throw err
+        }
+    },
+
+    /*
+        Deletes a singLe non-admin Comment's information on SQL Server  
+    */
+    deleteItem: async (config, privateKeyID, id) => {
+
+        try {
+            await sql.connect(config);
+
+            let query = ' DELETE US ';
+            query += ' FROM [ITCC_Comment] US ';
+            query += ' JOIN [ITCC_Website] WS (NOLOCK) ON (US.ITCC_WebsiteID = WS.ITCC_WebsiteID) ';
+            query += ' WHERE ( ' +
+                ' (US.ITCC_CommentID = @ID) AND (WS.PrivateKeyID = @PrivateKeyID) ' +
+                ') ';
+
+            const request = new sql.Request();
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+            request.input('ID', sql.Int, id);
+            const result = await request.query(query);
+            return result;
+
+        } catch (err) {
+            throw err
+        }
+    }
+};
+
+module.exports = CommentFunctions;
---
+++ routes/comment/index.js
@@ -0,0 +1,167 @@
+const router = require('express').Router();
+const configs = require('../../config_functions'); //require('../../config');
+const comments = require('./comment_functions');
+const SharedFunctions = require('../../shared/shared_functions');
+const LoginFunctions = require('../login/login_functions');
+
+const CommentFactory = {
+    Fields: {
+        ITCC_CommentID: 0, ITCC_WebsiteID: -1, ITCC_StatusID: -1, ITCC_UserID: -1, ITCC_PostID:1, ReplyPostID:-1,
+        CommentTitle: '', CommentDetail: '', CommentFullName: '',  PermaID: '', ReplyLevel:1, SortOrder:1,
+        CreateDate: new Date(), ModifyDate: new Date(), CreateAccountID: -1, ModifyAccountID: -1
+    }
+    , Set: function (value) {
+        this.Fields = Object.assign(this.Fields, value);
+    }
+    , Get: function () {
+        return this.Fields;
+    }
+};
+
+
+//  http://localhost:3010/api/gallery/FEA91F56-CBE3-4138-8BB6-62F9A5808D57/1
+//  http://localhost:3010/api/users/1DC52158-0175-479F-8D7F-D93FC7B1CAA4/page/1
+//  https://nodeapi.launchfeatures.com/api/gallery/88B8B45E-2BE7-44CB-BBEA-547BB9A8C7D5/2
+// get a paginated list of users
+router.get("/:siteid/page/:pagenum/:pagesize", async function (request, response) {
+    const siteid = request.params.siteid;
+    const authToken = LoginFunctions.getAuthenticationToken(request);
+    const authID = authToken || (request.headers.authid);
+    const pageNum = (request.params.pagenum) ? request.params.pagenum : 1;
+    const pageSize = (request.params.pagesize) ? request.params.pagesize : 10;
+    const offset = (pageNum - 1) * pageSize;
+
+    const config = configs.find(siteid); //(c => c.privateKeyID === siteid);
+    const roleNames = await LoginFunctions.getUserRolesByAuthToken(config, siteid, authID);
+    const itemsResult = await comments.getItems(config, siteid, offset, pageSize);
+    const result = itemsResult.recordset;
+
+    if (roleNames && roleNames.indexOf('admin') > -1) {
+        return response.status(200).send(result);
+    }
+    else {
+        // return a trimmed down result
+        return response.status(200).send(result);
+    }
+
+});
+
+// get one comment
+router.get("/:siteid/:id", async function (request, response) {
+    const siteid = request.params.siteid;
+    const authToken = LoginFunctions.getAuthenticationToken(request);
+    const authID = authToken || (request.headers.authid);
+    const id = request.params.id;
+
+    const config = configs.find(siteid); //(c => c.privateKeyID === siteid);
+    const roleNames = await LoginFunctions.getUserRolesByAuthToken(config, siteid, authID);
+    const authResult = await comments.getItem(config, siteid, id);
+    const result = (authResult.recordset && (authResult.recordset.length > 0))
+        ? authResult.recordset[0] : null;
+
+    if (roleNames && roleNames.indexOf('admin') > -1) {
+        return response.send(result);
+    }
+    else {
+        return response.send(result);
+    }
+});
+
+// get one comment by slug
+router.get("/:siteid/slug/:id", async function (request, response) {
+    const siteid = request.params.siteid;
+    const authToken = LoginFunctions.getAuthenticationToken(request);
+    const authID = authToken || (request.headers.authid);
+    const id = request.params.id;
+
+    const config = configs.find(siteid); //(c => c.privateKeyID === siteid);
+    const roleNames = await LoginFunctions.getUserRolesByAuthToken(config, siteid, authID);
+    const authResult = await comments.getItemBySlug(config, siteid, id);
+    const result = (authResult.recordset && (authResult.recordset.length > 0))
+        ? authResult.recordset[0] : null;
+
+    if (roleNames && roleNames.indexOf('admin') > -1) {
+        return response.send(result);
+    }
+    else {
+        return response.send(result);
+    }
+});
+
+// create a new Item along with some basic roles needed to access the system
+router.post("/:siteid", async function (request, response) {
+    const siteid = request.params.siteid;
+    const authToken = LoginFunctions.getAuthenticationToken(request);
+    const authID = authToken || (request.headers.authid);
+    const id = request.params.id;
+
+    const config = configs.find(siteid); //(c => c.privateKeyID === siteid);
+    const authUser = await LoginFunctions.getUserByAuthToken(config, siteid, authID);
+    //console.log({authUser: authUser});
+
+    if (authUser.RoleNames.indexOf('admin') > -1) {
+        CommentFactory.Set(request.body);
+        const dataValues = CommentFactory.Get();
+
+        if (!dataValues.Slug || dataValues.Slug.trim().length === 0) {
+            dataValues.Slug = SharedFunctions.slugify(dataValues.Name);
+        }
+
+        const authResult = await comments.createItem(config, siteid, authUser, dataValues);
+        const result = authResult;
+        return response.send(result);
+    }
+    else {
+        return response.status(401).send({ error: 'you\'re not authorized to access this' });
+    }
+
+});
+
+// update an Item
+router.put("/:siteid/:id", async function (request, response) {
+    const siteid = request.params.siteid;
+    const authToken = LoginFunctions.getAuthenticationToken(request);
+    const authID = authToken || (request.headers.authid);
+    const id = request.params.id;
+
+    const config = configs.find(siteid); //(c => c.privateKeyID === siteid);
+    const authUser = await LoginFunctions.getUserByAuthToken(config, siteid, authID);
+
+    if (authUser.RoleNames.indexOf('admin') > -1) {
+        CommentFactory.Set(request.body);
+        const dataValues = CommentFactory.Get();
+
+        if (!dataValues.Slug || dataValues.Slug.trim().length === 0) {
+            dataValues.Slug = SharedFunctions.slugify(dataValues.Name);
+        }
+
+        const authResult = await comments.updateItem(config, siteid, authUser, dataValues);
+        const result = authResult.recordset;
+        return response.send(result);
+    }
+    else {
+        return response.status(401).send({ error: 'you\'re not authorized to access this' });
+    }
+});
+
+// delete a Item
+router.delete("/:siteid/:id", async function (request, response) {
+    const siteid = request.params.siteid;
+    const authToken = LoginFunctions.getAuthenticationToken(request);
+    const authID = authToken || (request.headers.authid);
+    const id = request.params.id;
+
+    const config = configs.find(siteid); //(c => c.privateKeyID === siteid);
+    const authUser = await LoginFunctions.getUserByAuthToken(config, siteid, authID);
+
+    if (authUser.RoleNames.indexOf('admin') > -1) {
+        await comments.deleteItem(config, siteid, id);
+        return response.send(id);
+    }
+    else {
+        return response.status(401).send({ error: 'you\'re not authorized to access this' });
+    }
+});
+
+
+module.exports = router;
